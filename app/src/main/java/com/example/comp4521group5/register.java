5347bd675e39ec562d011918da4329922310b0d4
shadow1007 <steza777@gmail.com>
2019-05-17 19:00:30 +0800

    add database

--- app/src/main/java/com/example/comp4521group5/register.java
+++ app/src/main/java/com/example/comp4521group5/register.java
@@ -11,15 +11,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
-    EditText Email,Password,Password2;
-    Button registerButton,loginButton;
+     FirebaseFirestore db;
+     FirebaseAuth mAuth;
+    EditText Email, Password, Password2;
+    Button registerButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,6 @@ public class register extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-
         Email = (EditText) findViewById(R.id.username);
         Password = (EditText) findViewById(R.id.password);
         Password2 = (EditText) findViewById(R.id.password2);
@@ -40,58 +47,95 @@ public class register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String email = Email.getText().toString();
+                final  String dbEmail = Email.getText().toString();
                 String password = Password.getText().toString();
                 String password2 = Password2.getText().toString();
 
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Please fill in the required fields", Toast.LENGTH_SHORT).show();
                     return;
-                }
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
-                }
-
-                if(TextUtils.isEmpty(password2)){
-                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
-                }
-
-                if(password.length()<6){
-                    Toast.makeText(getApplicationContext(),"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
-                }
-
-                if(password.equals(password2)){
-                    Toast.makeText(getApplicationContext(),"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
-                }
-
-                mAuth.createUserWithEmailAndPassword(email,password2)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-//                                    FirebaseUser user = mAuth.getCurrentUser();
+                } else if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Please fill in the required fields", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (TextUtils.isEmpty(password2)) {
+                    Toast.makeText(getApplicationContext(), "Please fill in the required fields", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (!(password.equals(password2))) {
+                    Toast.makeText(getApplicationContext(), "Password must be the same"+password+password2, Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    mAuth.createUserWithEmailAndPassword(email, password2)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    final String userID=user.getUid();
 //                                    updateUI(user);
+                                       // addToDatabase(db,dbEmail);
+                                        db = FirebaseFirestore.getInstance();
 
-                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                                    finish();
+                                        Map<String, Object> userNew = new HashMap<>();
+                                        userNew.put("email", dbEmail);
+                                        userNew.put(userID,userID);
+                                        db.collection("users")
+                                                .add(userNew)
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+                                                        Toast.makeText(getApplicationContext(), "DocumentSnapshot added with ID: " + documentReference.getId(), Toast.LENGTH_SHORT).show();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(getApplicationContext(), "Error adding document", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                        Toast.makeText(getApplicationContext(), "createUserWithEmail:success", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(), Login.class));
+                                        finish();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), "createUserWithEmail:failure", Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                                else{
-                                    Toast.makeText(getApplicationContext(),"E-mail or password is wrong",Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                            });
+                }
             }
         });
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Login.class));
+                startActivity(new Intent(getApplicationContext(), Login.class));
             }
         });
 
-        if(mAuth.getCurrentUser()!=null){
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-        }
+//        if (mAuth.getCurrentUser() != null) {
+//            startActivity(new Intent(getApplicationContext(), Login.class));
+//        }
+    }
+    private void addToDatabase(FirebaseFirestore a,String e){
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", e);
+        a.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "DocumentSnapshot added with ID: " + documentReference.getId(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error adding document", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
 
